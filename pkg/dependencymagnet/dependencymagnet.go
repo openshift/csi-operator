d9da2039630cec01b13705d457fec27e48b10e12
Roman Bednar <rbednar@redhat.com>
2024-04-23 16:35:56 +0200

    drop: pull library-go deps to vendor for testing

--- pkg/dependencymagnet/dependencymagnet.go
+++ pkg/dependencymagnet/dependencymagnet.go
@@ -5,4 +5,6 @@ package dependencymagnet
 
 import (
 	_ "github.com/openshift/build-machinery-go"
+	_ "github.com/openshift/library-go/pkg/operator/apiserver/controller/workload"
+	_ "github.com/openshift/library-go/pkg/operator/status"
 )
