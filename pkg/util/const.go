84d28e17cf2a76ca0cabf369c57e10586a805f3a
Stephen Finucane <stephenfin@redhat.com>
2024-09-17 12:59:31 +0100

    Squashed 'legacy/openstack-cinder-csi-driver-operator/' content from commit dcac15afd
    git-subtree-dir: legacy/openstack-cinder-csi-driver-operator
    git-subtree-split: dcac15afd9dde042b120a6ee61c11206dccecba1

---
+++ pkg/util/const.go
@@ -0,0 +1,9 @@
+package util
+
+const (
+	// Operand and operator run in the same namespace
+	DefaultNamespace         = "openshift-cluster-csi-drivers"
+	OpenShiftConfigNamespace = "openshift-config"
+
+	CinderConfigName = "cloud-conf"
+)
