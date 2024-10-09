8a95ac04cfcd96cf9f274033a1103972f37470ba
Stephen Finucane <stephenfin@redhat.com>
2024-10-09 11:34:17 +0100

    openstack-cinder: Remove legacy operator code
    This is no longer used or referenced anywhere. Drop it:
    
      git rm -r legacy/openstack-cinder-csi-driver-operator
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- legacy/openstack-cinder-csi-driver-operator/pkg/util/const.go
+++
@@ -1,9 +0,0 @@
-package util
-
-const (
-	// Operand and operator run in the same namespace
-	DefaultNamespace         = "openshift-cluster-csi-drivers"
-	OpenShiftConfigNamespace = "openshift-config"
-
-	CinderConfigName = "cloud-conf"
-)
