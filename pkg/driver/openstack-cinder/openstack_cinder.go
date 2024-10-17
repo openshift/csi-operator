0208eff59e33b55d9f87d256e97653e55735b6bb
Stephen Finucane <stephenfin@redhat.com>
2024-10-17 15:54:15 +0100

    openstack-cinder: Sync config map to correct namespace
    In a standalone deployment, we sync these config maps to the
    'openshift-cluster-csi-drivers' namespace. That's okay because there's
    only one configuration to be worried about. However, if we did this in a
    HCP deployment then every managed cluster would try to lay down the CM
    in the same place. Disaster!
    
    Fortunately, we have a much better namespace to sync into here. The
    guest namespace. Make it so.
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- pkg/driver/openstack-cinder/openstack_cinder.go
+++ pkg/driver/openstack-cinder/openstack_cinder.go
@@ -117,7 +117,11 @@ func GetOpenStackCinderOperatorConfig() *config.OperatorConfig {
 
 // GetOpenStackCinderOperatorControllerConfig returns second half of runtime configuration of the CSI driver operator,
 // after a client connection + cluster flavour are established.
-func GetOpenStackCinderOperatorControllerConfig(ctx context.Context, flavour generator.ClusterFlavour, c *clients.Clients) (*config.OperatorControllerConfig, error) {
+func GetOpenStackCinderOperatorControllerConfig(
+	ctx context.Context,
+	flavour generator.ClusterFlavour,
+	c *clients.Clients,
+) (*config.OperatorControllerConfig, error) {
 	if flavour != generator.FlavourStandalone {
 		klog.Error(nil, "Flavour HyperShift is not supported!")
 		return nil, fmt.Errorf("Flavour HyperShift is not supported!")
@@ -133,7 +137,7 @@ func GetOpenStackCinderOperatorControllerConfig(ctx context.Context, flavour gen
 
 	cfg.DeploymentWatchedSecretNames = append(cfg.DeploymentWatchedSecretNames, cloudCredSecretName, metricsCertSecretName)
 
-	configMapSyncer, err := createConfigMapSyncer(c)
+	configMapSyncer, err := createConfigMapSyncer(c, flavour)
 	if err != nil {
 		return nil, err
 	}
@@ -195,12 +199,23 @@ func withConfigDaemonSetHook(c *clients.Clients) (csidrivernodeservicecontroller
 // createConfigMapSyncer syncs config maps containing configuration for the CSI driver from the
 // user-managed namespace to the operator namespace, validating it and potentially transforming it
 // in the process
-func createConfigMapSyncer(c *clients.Clients) (factory.Controller, error) {
+func createConfigMapSyncer(c *clients.Clients, flavour generator.ClusterFlavour) (factory.Controller, error) {
+	// The "operator namespace" is different depending on the type of deployment.
+	// For a standalone deployment, it's the same namespace every other component is running in.
+	// For a hypershift deployment, it's the guest cluster's namespace.
+	var controlPlaneNamespace string
+	if flavour == generator.FlavourStandalone {
+		controlPlaneNamespace = clients.CSIDriverNamespace
+	} else {
+		controlPlaneNamespace = c.ControlPlaneNamespace
+	}
+
 	configSyncController := configsync.NewConfigSyncController(
 		c.OperatorClient,
 		c.KubeClient,
 		c.KubeInformers,
 		c.ConfigInformers,
+		controlPlaneNamespace,
 		resyncInterval,
 		c.EventRecorder)
 
