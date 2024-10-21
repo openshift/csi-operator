54b58412da9ee2526d85bccf23eb9b9bdac9776a
Maysa Macedo <maysa.macedo95@gmail.com>
2024-10-21 16:42:37 -0300

    Add new starter controller
    Manila needs to run a check before running
    any controller. In order to accomodate this,
    a new controller named `StarterController` is
    being added. Every 20 mins this controller runs
    a pre-condition check that if satisfied runs all
    controllers and if not, sets a disable condition
    to the operator.

--- pkg/operator/config/config.go
+++ pkg/operator/config/config.go
@@ -74,6 +74,12 @@ type OperatorControllerConfig struct {
 
 	// ExtraReplacements defines additional replacements that should be made to assets
 	ExtraReplacementsFunc func() []string
+
+	// Precondition to run operator
+	Precondition func() (bool, error)
+
+	// List of informers that should be added to the precondition controller
+	PreconditionInformers []factory.Informer
 }
 
 func (o *OperatorControllerConfig) AddCredentialsRequestHook(hook credentialsrequestcontroller.CredentialsRequestHook) {
